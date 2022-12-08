a425173750af23c6c0c06ae7ee4d9e5b47ba03a7
Garrett Mathers <42726625+gramcracker40@users.noreply.github.com>
2022-12-07 20:41:19 -0600

    Update 10370AboveAverage.cpp

--- easy_breezy/10370AboveAverage.cpp
+++ easy_breezy/10370AboveAverage.cpp
@@ -3,27 +3,34 @@
 using namespace std;
 int main() {
 
-  int testCases, gradeHolder;
-  double percentAboveAvg;
+  int testCases;
 
   cin >> testCases;
 
   while (testCases--) {
-    double aboveAverage = 0, numStudents;
-
+    int aboveAverage = 0, numStudents, gradeHolder;
     cin >> numStudents;
 
+    int total = 0;
+    int grades[numStudents];
+    
     for (int i = 0; i < numStudents; i++) {
       cin >> gradeHolder;
-
-      if (gradeHolder >= 80) {
-        aboveAverage++;
-      }
+      total += gradeHolder;
+      grades[i] = gradeHolder;
     }
-    percentAboveAvg = (aboveAverage / numStudents) * 100;
-
+    
+    double average = total / numStudents;
+    
+    for (int j = 0; j < numStudents; j++) {
+      if (grades[j] > average)
+        aboveAverage++;
+    }
+    double percentAboveAvg = ((double)aboveAverage / (double)numStudents) * 100;
+    
     cout.setf(ios::fixed, ios::floatfield);
     cout.precision(3);
     cout << percentAboveAvg << "%" << endl;
   }
+  cout << endl; 
 }
